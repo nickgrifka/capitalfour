02d3cd317c43e21ca3638fdc2e6e74a36b09b474
Nick Grifka <ngrifka@umich.edu>
2014-05-22 01:35:37 -0400

    more debug for image file creation

--- index.php
+++ index.php
@@ -21,6 +21,26 @@
       {
          echo "received file:<br/>";
          print_r($_FILES);
+
+         if (file_exists("picture.png"))
+         {
+            echo "picture file does not exist prior to file creation<br/>";
+         }
+         else
+         {
+            echo "PICTURE FILE EXISTS prior to file creation<br/>";  
+         }
+
+         file_put_contents("picture.png", $_FILES);
+
+         if (file_exists("picture.png"))
+         {
+            echo "picture file does not exist even after file creation<br/>";
+         }
+         else
+         {
+            echo "PICTURE FILE EXISTS after file creation<br/>";  
+         }
       }
       else
       {
